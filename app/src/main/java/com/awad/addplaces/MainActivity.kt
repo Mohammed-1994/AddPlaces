110d4ebd2ae642eefba0f050fe0c02ff93f2c59d
meroo <merooa937@gmail.com>
2021-04-17 17:30:53 +0300

    get started

--- app/src/main/java/com/awad/addplaces/MainActivity.kt
+++ app/src/main/java/com/awad/addplaces/MainActivity.kt
@@ -3,35 +3,28 @@ package com.awad.addplaces
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 
 private const val TAG = "MainActivity myTag"
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
 
     @Inject
-    lateinit var firestore: FirebaseFirestore
+    lateinit var fireStore: FirebaseFirestore
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        firestore.collection("data").document("one")
-            .update("stringExample", "hi world")
-            .addOnCompleteListener { snapshot ->
-                Log.d(TAG, "onCreate: completed")
-                if (snapshot.isSuccessful)
-                    Log.d(TAG, "onCreate: success ")
-                else
-                    Log.d(TAG, "onCreate: Error ", snapshot.exception)
-            }
 
     }
 }
---
+++ app/src/main/java/com/awad/addplaces/util/Test.kt
@@ -0,0 +1,91 @@
+package com.awad.addplaces.util
+
+import android.util.Log
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+import javax.inject.Inject
+
+private const val TAG = "Test myTag"
+
+class Test : SaveReference {
+
+    @Inject
+    lateinit var fireStore: FirebaseFirestore
+    private fun addData() {
+        val docData = hashMapOf(
+            "stringExample" to "Hello world!",
+            "booleanExample" to true,
+            "numberExample" to 3.14159265,
+            "dateExample" to Timestamp(Date()),
+            "listExample" to arrayListOf(1, 2, 3),
+            "nullExample" to null,
+            "Ref" to fireStore
+
+
+        )
+
+        val nestedData = hashMapOf(
+            "a" to 5,
+            "b" to true
+        )
+
+        docData["objectExample"] = nestedData
+
+
+        fireStore.collection("data")
+            .add(nestedData)
+            .addOnSuccessListener {
+                Log.d(TAG, "DocumentSnapshot successfully written!")
+                saveRef(it, it.id)
+                Log.d(TAG, "success: id =  ${it.id}")
+
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+    }
+
+    override fun saveRef(ref: DocumentReference, id: String) {
+        Log.d(TAG, "saveRef: id =  $id")
+        val nestedData = hashMapOf(
+            "reg" to ref,
+
+            )
+        fireStore.collection("new created").document(id).set(nestedData)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    var newRef = fireStore.collection("new created").document(id)
+                    getRefData(newRef)
+                    Log.d(TAG, "saveRef: success")
+                } else
+                    Log.e(TAG, "saveRef: Error", it.exception)
+            }
+    }
+
+    override fun getRefData(newRef: DocumentReference) {
+        newRef.get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                val ref: DocumentReference = it.result?.get("reg") as DocumentReference
+                readLastRef(ref)
+            }
+
+        }
+    }
+
+    override fun readLastRef(last: DocumentReference) {
+        last.get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d(TAG, "readLastRef: ${it.result?.get("a")}")
+                Log.d(TAG, "readLastRef: ${it.result?.get("b")}")
+            }
+        }
+    }
+}
+
+interface SaveReference {
+    fun saveRef(ref: DocumentReference, id: String)
+    fun getRefData(newRef: DocumentReference)
+    fun readLastRef(last: DocumentReference)
+}
+
